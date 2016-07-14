1707f57750d137d6629162d1e94a2204d69b38d4
许绍娟 <xushaojuan@xushaojuandeMacBook-Pro.local>
2016-07-14 23:47:00 +0800

    添加项目工程

---
+++ CloudChat/CloudChat/Bridge.m
@@ -0,0 +1,9 @@
+//
+//  Bridge.m
+//  CloudChat
+//
+//  Created by 许绍娟 on 16/7/14.
+//  Copyright © 2016年 许绍娟. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
---
+++ CloudChat/CloudChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CloudChat
+//
+//  Created by 许绍娟 on 16/7/14.
+//  Copyright © 2016年 许绍娟. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
